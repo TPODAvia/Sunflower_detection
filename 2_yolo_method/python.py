d0e66af1ccddaedac7810423f76432da1ac86883
TPODAvia <thepowerofdarknes2000@gmail.com>
2023-10-30 17:50:46 +0300

    yolo update

--- 2_yolo_method/python.py
+++ 2_yolo_method/python.py
@@ -1,69 +1,47 @@
-# importing required libraries
 import os
-import shutil
-import random
-import tqdm
+import cv2
+import csv
 
-# update these paths as per your local system
-train_path_img = "./yolo_data/images/train/"
-train_path_label = "./yolo_data/labels/train/"
-val_path_img = "./yolo_data/images/val/"
-val_path_label = "./yolo_data/labels/val/"
-test_path = "./yolo_data/test"
+def write_to_file(img_path, bbox):
+    img_name = os.path.basename(img_path)
+    img_name = os.path.splitext(img_name)[0]
+    txt_name = os.path.join("/home/gumich/Sunflower_detection/test/", 'name.txt')
+    with open(txt_name, 'a') as f:
+        f.write(f"0 {bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]}\n")
 
-def train_test_split(path, neg_path=None, split = 0.2):
-    print("------ PROCESS STARTED -------")
 
-    files = list(set([name[:-4] for name in os.listdir(path)])) ## removing duplicate names i.e. counting only number of images
+def convert(size, box):
+    dw = 1./size[0]
+    dh = 1./size[1]
+    x = (box[0] + box[1])/2.0
+    y = (box[2] + box[3])/2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return (x,y,w,h)
 
-    print (f"--- This folder has a total number of {len(files)} images---")
-    random.seed(42)
-    random.shuffle(files)
 
-    test_size = int(len(files) * split)
-    train_size = len(files) - test_size
+with open('/home/gumich/Sunflower_detection/annotations.csv', 'r') as file:
+    data = csv.reader(file)
 
-    ## creating required directories
-    os.makedirs(train_path_img, exist_ok = True)
-    os.makedirs(train_path_label, exist_ok = True)
-    os.makedirs(val_path_img, exist_ok = True)
-    os.makedirs(val_path_label, exist_ok = True)
+    for item in data:
 
-    ### ----------- copying images to train folder
-    for filex in tqdm.tqdm(files[:train_size]):
-        if filex == 'classes':
-            continue
-        shutil.copy2(path + filex + '.jpg',f"{train_path_img}/" + filex + '.jpg' )
-        shutil.copy2(path + filex + '.txt', f"{train_path_label}/" + filex + '.txt')
+        img_path = item[0]
+        ymin = int(item[1])
+        xmin = int(item[2])
+        ymax = int(item[3])
+        xmax = int(item[4])
+        
+        # Get image dimensions
+        # im = cv2.imread(img_path)
+        w= 5472
+        h= 3078
 
-    print(f"------ Training data created with 80% split {len(files[:train_size])} images -------")
+        # Convert bounding box format
+        bbox = convert((w,h), [xmin, xmax, ymin, ymax])
 
-    if neg_path:
-        neg_images = list(set([name[:-4] for name in os.listdir(neg_path)])) ## removing duplicate names i.e. counting only number of images
-        for filex in tqdm.tqdm(neg_images):
-            shutil.copy2(neg_path+filex+ ".jpg", f"{train_path_img}/" + filex + '.jpg')
-
-        print(f"------ Total  {len(neg_images)} negative images added to the training data -------")
-
-        print(f"------ TOTAL Training data created with {len(files[:train_size]) + len(neg_images)} images -------")
-
-    ### copying images to validation folder
-    for filex in tqdm.tqdm(files[train_size:]):
-        if filex == 'classes':
-            continue
-        shutil.copy2(path + filex + '.jpg', f"{val_path_img}/" + filex + '.jpg' )
-        shutil.copy2(path + filex + '.txt', f"{val_path_label}/" + filex + '.txt')
-
-    print(f"------ Testing data created with a total of {len(files[train_size:])} images ----------")
-
-    print("------ TASK COMPLETED -------")
-
-# spliting the data into train-test and creating train.txt and test.txt files
-train_test_split('/content/drive/MyDrive/custom_notebooks/yolo_data/')
-
-### for label_tag
-# train_test_split('/content/drive/MyDrive/yolov8/data/') ### without negative images
-# train_test_split('./data/','./negative_images/') ### if you want to feed negative images
-
-# import ultralytics
-# ultralytics.checks()
+        # Write to file
+        write_to_file("/home/gumich/Sunflower_detection/test/", bbox)
